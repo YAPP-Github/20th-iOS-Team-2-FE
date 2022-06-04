10a3a4bffe91bacfe46f0cb3d6d477c0d0ad4c2c
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-04 20:40:09 +0900

    [FEAT] Tab

---
+++ Sofa/Sources/View/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .home
+    
+    enum Tab {
+        case home
+        case calendar
+        case album
+        case setting
+    }
+    
+    var body: some View {
+        TabView(selection: $selection) {
+//            HomeView()
+            Color.blue
+                .tabItem {
+                    Image(systemName: "person.3.fill")
+                    Text("홈")
+                }
+                .tag(Tab.home)
+            Color.green
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("캘린더")
+                }
+                .tag(Tab.calendar)
+            Color.red
+                .tabItem {
+                    Image(systemName: "book.closed")
+                    Text("엘범")
+                }
+                .tag(Tab.album)
+            Color.yellow
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tab.setting)
+        }
+        .accentColor(.black)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

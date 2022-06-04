f822452320db6cef9b8f9a16226b0aa65aaee1c8
임주민 <limj1213@gmail.com>
2022-06-04 22:50:42 +0900

    [ADD] : 프로젝트 폴더 다시 추가

---
+++ Sofa/Configuration/Base/Base.swift
@@ -0,0 +1,8 @@
+//
+//  Base.swift
+//  Sofa
+//
+//  Created by 임주민 on 2022/06/04.
+//
+
+import Foundation
---
+++ Sofa/Sources/View/Album/AlbumView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumView.swift
+//  Sofa
+//
+//  Created by 임주민 on 2022/06/04.
+//
+
+import SwiftUI
+
+struct AlbumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumView()
+    }
+}

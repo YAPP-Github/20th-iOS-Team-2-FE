1413fbb24840df14478aee7e6a9240fca9031c5e
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-04 19:50:50 +0900

    [INIT]: Project init

---
+++ Sofa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sofa/SofaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SofaApp.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/04.
+//
+
+import SwiftUI
+
+@main
+struct SofaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

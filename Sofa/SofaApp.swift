2fbd5d3fb2a96d2998018d12d42cff8373ff1523
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-06-04 21:12:23 +0900

    [CHORE] INDENT = 2 spaces

--- Sofa/SofaApp.swift
+++ Sofa/SofaApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct SofaApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }

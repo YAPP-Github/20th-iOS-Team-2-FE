6660f27e309a5db470805a9c69c374923feb68c5
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-08 10:56:32 +0900

    [FEAT] Dummy AlbumDetailView

---
+++ Sofa/Sources/View/Album/SubViews/AlbumDetailView/AlbumDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumDetailView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/07.
+//
+
+import SwiftUI
+
+struct AlbumDetailView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct AlbumDetailView_Previews: PreviewProvider {
+  static var previews: some View {
+    AlbumDetailView()
+  }
+}

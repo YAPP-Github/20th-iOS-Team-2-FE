75b5826eed95c918e74ed86f4c7d088472de4f23
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-08 11:07:26 +0900

    [FEAT] NavigationLink를 통한 AlbumDetailView로 이동

--- Sofa/Sources/View/Album/AlbumView.swift
+++ Sofa/Sources/View/Album/AlbumView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct AlbumView: View {
+  @State var albums = [Album]()
+  
   var body: some View {
     NavigationView {
-      ScrollView {
-        
+      List(albums, id: \.albumId) { album in
+        ZStack {
+          NavigationLink(destination: AlbumDetailView()) {
+          }
           AlbumRow(album: album)
+        }
       }
       .background(Color.red)
       .navigaionBarWithButtonStyle("앨범")
@@ -22,6 +27,11 @@ struct AlbumView: View {
 
 struct AlbumView_Previews: PreviewProvider {
   static var previews: some View {
-    AlbumView()
+    let albums = [
+      Album(albumId: 0, title: "2022-12-25 앨범", thumbnail: "", date: "2022-06-05"),
+      Album(albumId: 1, title: "제주도 가족여행", thumbnail: "", date: "2022-05-28"),
+      Album(albumId: 2, title: "여의도 공원 나드리", thumbnail: "", date: "2022-05-28")
+    ]
+    AlbumView(albums: albums)
   }
 }

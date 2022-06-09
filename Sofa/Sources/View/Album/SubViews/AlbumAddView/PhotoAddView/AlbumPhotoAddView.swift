a554531b4018edf0a7e1d5b17cef92d408f3f27c
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-10 07:57:11 +0900

    [FEAT] photo add 구현

---
+++ Sofa/Sources/View/Album/SubViews/AlbumAddView/PhotoAddView/AlbumPhotoAddView.swift
@@ -0,0 +1,37 @@
+//
+//  AlbumPhotoAddView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/09.
+//
+
+import SwiftUI
+
+struct AlbumPhotoAddView: View {
+  @State private var isTabPhoto = false
+  var photoList: [String] = MockData().photoList
+  
+  var body: some View {
+    ScrollView {
+      VStack(spacing: 0) {
+        ZStack {
+          Color.black // 배경
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/2) // 화면의 반
+          Image("photo01")
+            .resizable()
+            .scaledToFit()
+            .frame(height: UIScreen.main.bounds.height/2) // 화면의 반
+            .padding(.all, 2)
+        }
+        AlbumPhotoAddList(photoList: photoList)
+      }
+    }
+    .animation(.spring(response: 1, dampingFraction: 0.7, blendDuration: 0))
+  }
+}
+
+struct AlbumPhotoAddView_Previews: PreviewProvider {
+  static var previews: some View {
+    AlbumPhotoAddView(photoList: MockData().photoList)
+  }
+}

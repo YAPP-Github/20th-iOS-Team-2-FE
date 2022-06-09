06ad06e1f156cba6b8023c073bc83a389c1e1b0f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-09 22:58:34 +0900

    [FEAT] tabBar 숨기기/표시 2가지 방법 - Extension, ViewModifier 추가
    1) View().setTabBarVisibility(isHidden : Bool)
    2) UITabBar.toogleTabBarVisibility() 를 호출

---
+++ Sofa/Configuration/Extensions/Ex+View.swift
@@ -0,0 +1,38 @@
+//
+//  Ex+View.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/09.
+//
+
+import protocol SwiftUI.View
+import struct SwiftUI.AnyView
+
+public extension View {
+  /// 탭바 숨김 처리 여부
+  /// - Parameter isHidden:
+  /// - Returns:
+  func setTabBarVisibility(isHidden : Bool) -> some View {
+    background(TabBarAccessor { tabBar in
+      // print(">> TabBar height: \(tabBar.bounds.height)")
+      // !! use as needed, in calculations, @State, etc.
+      // 혹은 높이를 변경한다던지 여러가지 설정들이 가능하다.
+      tabBar.isHidden = isHidden
+    })
+  }
+  
+  func showTabBar(animated: Bool = true) -> some View {
+    return self.modifier(ShowTabBar(animated: animated))
+  }
+  func hideTabBar(animated: Bool = true) -> some View {
+    return self.modifier(HiddenTabBar(animated: animated))
+  }
+  
+  func shouldHideTabBar(_ hidden: Bool, animated: Bool = true) -> AnyView {
+    if hidden {
+      return AnyView(hideTabBar(animated: animated))
+    } else {
+      return AnyView(showTabBar(animated: animated))
+    }
+  }
+}
---
+++ Sofa/Configuration/ViewModifiers/HiddenTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  HiddenTabBar.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/09.
+//
+
+import SwiftUI
+
+struct HiddenTabBar: ViewModifier {
+    var animated = true
+
+    func body(content: Content) -> some View {
+        return content.padding(.zero).onAppear {
+            UITabBar.hideTabBar(animated: animated)
+        }
+    }
+}

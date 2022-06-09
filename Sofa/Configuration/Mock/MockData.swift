8534f39e6b6a005d3a188e6963463eb9afa715e3
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-10 05:12:21 +0900

    [FEAT] mockup data helper

--- Sofa/Configuration/Mock/MockData.swift
+++ Sofa/Configuration/Mock/MockData.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct MockData {
+  
+  var photoList: [String] = [
+    "photo01", "photo02", "photo03", "photo04", "photo05",
+    "photo06", "photo07", "photo08", "photo09", "photo10"
+  ]
+}

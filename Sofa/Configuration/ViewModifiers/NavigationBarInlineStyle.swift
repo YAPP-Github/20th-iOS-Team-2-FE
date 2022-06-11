9f1359d995483eefc458f1f66edc3e845041a98b
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-11 20:20:59 +0900

    [FEAT] navigationBarInlineStyle 추가 (tabBarItem 색상 변경 가능)
    isCancleClick: 취소 버튼의 action을 위한 Binding<Bool>
    isNextClick: 다음 버튼의 action을 위한 Binding<Bool>
    buttonColor: tabBarItem 색상 변경 (default : UIColor.label)
    title: navigation title 변경 가능 (default : "")

--- Sofa/Configuration/ViewModifiers/NavigationBarInlineStyle.swift
+++ Sofa/Configuration/ViewModifiers/NavigationBarInlineStyle.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 
 struct NavigationBarInlineStyle: ViewModifier {
-  
+  @Binding var isCancleClick: Bool
+  @Binding var isNextClick: Bool
+  var buttonColor: Color
+  var title: String
+
   func body(content: Content) -> some View {
     return content
-      
+      .navigationBarItems(
+        leading: Button(action: {
+          isCancleClick = true
+        }, label: {
+          HStack(spacing: 0) {
+            Image(systemName: "chevron.left")
+            Text("취소")
+          }
+          .foregroundColor(buttonColor)
+        })
+        .accentColor(.black)
+        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)),
+        trailing: Button(action: {
+          isNextClick = true
+        }, label: {
+          HStack(spacing: 0) {
+            Text("다음")
+            Image(systemName: "chevron.right")
+          }
+          .foregroundColor(buttonColor)
+        })
+        .accentColor(.black)
+        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+      )
+      .navigationBarTitleDisplayMode(.inline)
+      .navigationTitle(title)
+      .onAppear {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor =
+        UIColor(white: 1, alpha: 0.6)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+      }
   }
 }
 
@@ -19,6 +57,7 @@ struct NavigationBarInlineStyle_Previews: PreviewProvider {
   static var previews: some View {
     NavigationView {
       Color.gray.edgesIgnoringSafeArea(.all)
+        .navigationBarInlineStyle(isCancleClick: .constant(true), isNextClick: .constant(true), buttonColor: Color.init(hex: "#43A047"), "제목")
     }
   }
 }
--- Sofa/Sources/View/Album/SubViews/AlbumAddView/PhotoAddView/AlbumPhotoAddView.swift
+++ Sofa/Sources/View/Album/SubViews/AlbumAddView/PhotoAddView/AlbumPhotoAddView.swift
@@ -13,26 +13,29 @@ struct AlbumPhotoAddView: View {
   @State var imageClick: UIImage?
   
   var body: some View {
-    VStack(spacing: 0) {
-      ZStack {
-        Color.black // 배경
-          .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.5) // 화면의 반
-        
-        if imageClick != nil { // 첫 Appear상태에는 선택된 이미지가 없음
-          Image(uiImage: imageClick!)
-            .resizable()
-            .scaledToFit()
-            .frame(height: UIScreen.main.bounds.height * 0.5) // 화면의 반
-            .padding(.all, 2)
-            .pinchToZoom()
+    NavigationView {
+      VStack(spacing: 0) {
+        ZStack {
+          Color.black // 배경
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.5) // 화면의 반
+          
+          if imageClick != nil { // 첫 Appear상태에는 선택된 이미지가 없음
+            Image(uiImage: imageClick!)
+              .resizable()
+              .scaledToFit()
+              .frame(height: UIScreen.main.bounds.height * 0.5) // 화면의 반
+              .padding(.all, 2)
+              .pinchToZoom()
+          }
         }
+        
+        ScrollView {
+          AlbumPhotoAddList(imageClick: $imageClick)
+            .frame(height:UIScreen.main.bounds.height * 0.5)
+        }
+        .animation(.spring(response: 1, dampingFraction: 0.7, blendDuration: 0))
       }
-      
-      ScrollView {
-        AlbumPhotoAddList(imageClick: $imageClick)
-          .frame(height:UIScreen.main.bounds.height * 0.5)
-      }
-      .animation(.spring(response: 1, dampingFraction: 0.7, blendDuration: 0))
+      .navigationBarInlineStyle(isCancleClick: .constant(true), isNextClick: .constant(true), buttonColor: Color.init(hex: "#43A047"), "사진 선택") // 임시 컬러
     }
   }
 }

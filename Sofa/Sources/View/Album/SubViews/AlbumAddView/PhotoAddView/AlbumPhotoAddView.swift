06879566ea88e8fdd1a2d119f7b19d02b0b2faac
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-11 18:55:20 +0900

    [CHORE] 들여쓰기 정렬

--- Sofa/Sources/View/Album/SubViews/AlbumAddView/PhotoAddView/AlbumPhotoAddView.swift
+++ Sofa/Sources/View/Album/SubViews/AlbumAddView/PhotoAddView/AlbumPhotoAddView.swift
@@ -11,19 +11,20 @@ struct AlbumPhotoAddView: View {
   @State private var isTabPhoto = false
   @State private var image : Image?
   @State var imageClick: UIImage?
-
+  
   var body: some View {
     VStack(spacing: 0) {
       ZStack {
         Color.black // 배경
           .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.5) // 화면의 반
-
+        
         if imageClick != nil { // 첫 Appear상태에는 선택된 이미지가 없음
           Image(uiImage: imageClick!)
             .resizable()
             .scaledToFit()
             .frame(height: UIScreen.main.bounds.height * 0.5) // 화면의 반
             .padding(.all, 2)
+            .pinchToZoom()
         }
       }
       

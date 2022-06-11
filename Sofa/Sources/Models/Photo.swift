8e0b128e412ad4fbd02e511d4764bb50c26a83da
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-12 01:56:13 +0900

    [ADD] Photo Model 정의

---
+++ Sofa/Sources/Models/Photo.swift
@@ -0,0 +1,15 @@
+//
+//  Photo.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/12.
+//
+
+import Foundation
+import SwiftUI
+import Photos
+
+struct Photo: Hashable {
+  var isSelect: Bool
+  var asset: Asset
+}

bb10e88940a73f371c5e19c4daaa275bb5d7451e
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-12 00:15:01 +0900

    [FEAT] Row에 select circle view 구현

--- Sofa/Sources/View/Album/SubViews/AlbumAddView/PhotoAddView/SubViews/Row/AlbumPhotoAddRow.swift
+++ Sofa/Sources/View/Album/SubViews/AlbumAddView/PhotoAddView/SubViews/Row/AlbumPhotoAddRow.swift
@@ -12,26 +12,43 @@ struct AlbumPhotoAddRow: View {
   @ObservedObject var photo: Asset
   @Binding var imageClick: UIImage?
   var index: Int
+  let size = UIScreen.main.bounds.width * 0.325
   
   var body: some View {
     Button(action: {
       imageClick = photo.image!
     }, label: {
       if photo.image != nil {
-        Image(uiImage: photo.image!)
-          .resizable()
-          .scaledToFill()
-          .frame(
-            width: UIScreen.main.bounds.width * 0.325,
-            height: UIScreen.main.bounds.width * 0.325,
-            alignment: .center)
-          .cornerRadius(5.0)
+        ZStack {
+          Image(uiImage: photo.image!)
+            .resizable()
+            .scaledToFill()
+            .frame(
+              width: size,
+              height: size,
+              alignment: .center)
+            .cornerRadius(5.0)
+          
+          Circle()
+            .strokeBorder(Color.white, lineWidth: 1)
+            .background(Color(UIColor.gray.withAlphaComponent(0.6)))
+            .frame(width: 24, height: 24, alignment: .center)
+            .offset(x: size/3, y: -(size/3))
+        }
       } else {
-        Color.black
-          .frame(
-            width: UIScreen.main.bounds.width * 0.325,
-            height: UIScreen.main.bounds.width * 0.325)
-          .cornerRadius(5.0)
+        ZStack {
+          Color.black
+            .frame(
+              width: size,
+              height: size)
+            .cornerRadius(5.0)
+          
+          Circle()
+            .strokeBorder(Color.white, lineWidth: 1)
+            .background(Circle().foregroundColor(Color(UIColor.gray.withAlphaComponent(0.6))))
+            .frame(width: 24, height: 24, alignment: .center)
+            .offset(x: size/3, y: -(size/3))
+        }
       }
     })
     .onAppear {

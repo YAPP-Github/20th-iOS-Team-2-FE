61d81a6252f153c47d7688ac8a26e4d0fdac5032
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-12 17:08:54 +0900

    [ADD] NavigationBar with text button style

---
+++ Sofa/Configuration/ViewModifiers/NavigationBarWithTextButtonStyle.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBarWithTextButtonStyle.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/12.
+//
+
+import SwiftUI
+
+struct NavigationBarWithTextButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationBarWithTextButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarWithTextButtonStyle()
+    }
+}

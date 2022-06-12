dd57e428478ca708b1266f8ea1be8d9b9b333623
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-12 17:31:43 +0900

    [ADD] 앨범 upload 중 날짜 선택 view

---
+++ Sofa/Sources/View/Album/SubViews/AlbumSelectDateView/AlbumSelectDateView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumSelectDateView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/12.
+//
+
+import SwiftUI
+
+struct AlbumSelectDateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumSelectDateView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumSelectDateView()
+    }
+}

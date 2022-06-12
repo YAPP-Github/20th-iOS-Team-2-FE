181fe59c6ff4547c1d6b59ba230493d71dc21cc3
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-12 23:26:40 +0900

    [CHORE] 구조 변경, VStack으로 묶음

--- Sofa/Sources/View/Album/SubViews/AlbumSelectDateView/AlbumSelectDateView.swift
+++ Sofa/Sources/View/Album/SubViews/AlbumSelectDateView/AlbumSelectDateView.swift
@@ -12,8 +12,10 @@ struct AlbumSelectDateView: View {
   
   var body: some View {
     NavigationView {
-      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        .navigationBarWithTextButtonStyle(isNextClick: $isNext, isDisalbeNextButton: .constant(true), "사진 올리기", nextText: "올리기", Color.init(hex: "#43A047"))
+      VStack {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      }
+      .navigationBarWithTextButtonStyle(isNextClick: $isNext, isDisalbeNextButton: .constant(false), "사진 올리기", nextText: "올리기", Color.init(hex: "#43A047"))
     }
     .navigationViewStyle(StackNavigationViewStyle())
     .navigationBarHidden(true)

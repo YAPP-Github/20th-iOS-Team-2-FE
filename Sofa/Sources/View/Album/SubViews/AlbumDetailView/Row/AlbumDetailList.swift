2d52bafe4139814b76144b292396537ebe2118ed
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-13 00:57:57 +0900

    [ADD] Album detail list

---
+++ Sofa/Sources/View/Album/SubViews/AlbumDetailView/Row/AlbumDetailList.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumDetailList.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/13.
+//
+
+import SwiftUI
+
+struct AlbumDetailList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumDetailList_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumDetailList()
+    }
+}

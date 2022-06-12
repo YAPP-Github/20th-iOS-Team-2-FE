5cc5d1bc91d3b6a8b209ca650de996b261f4e33f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-12 17:33:08 +0900

    [FEAT] 사진 올리기 View (except 캘린더, only navigaion bar)

--- Sofa/Sources/View/Album/SubViews/AlbumSelectDateView/AlbumSelectDateView.swift
+++ Sofa/Sources/View/Album/SubViews/AlbumSelectDateView/AlbumSelectDateView.swift
@@ -8,13 +8,18 @@
 import SwiftUI
 
 struct AlbumSelectDateView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State var isNext = false
+  
+  var body: some View {
+    NavigationView {
+      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        .navigationBarWithTextButtonStyle(isNextClick: $isNext, isDisalbeNextButton: .constant(true), "사진 올리기", nextText: "올리기", Color.init(hex: "#43A047"))
     }
+  }
 }
 
 struct AlbumSelectDateView_Previews: PreviewProvider {
-    static var previews: some View {
-        AlbumSelectDateView()
-    }
+  static var previews: some View {
+    AlbumSelectDateView()
+  }
 }

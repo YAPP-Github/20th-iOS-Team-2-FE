00a272c4198c2b0c3be10b1bd320d99affdc1b1c
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-12 15:51:01 +0900

    [ADD] Album 유형별 View

---
+++ Sofa/Sources/View/Album/SubViews/Row/AlbumTypeRow.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumTypeRow.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/12.
+//
+
+import SwiftUI
+
+struct AlbumTypeRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumTypeRow_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumTypeRow()
+    }
+}

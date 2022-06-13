b29c597d1f890889c38eb896a7eaeb45b9a6fdd3
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-06-14 02:11:29 +0900

    [Feat] Title, Lottie Animation

---
+++ Sofa/Sources/View/Welcome/LoginSubViews/LoginButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginButtonView.swift
+//  Sofa
+//
+//  Created by 양유진 on 2022/06/14.
+//
+
+import SwiftUI
+
+struct LoginButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginButtonView()
+    }
+}

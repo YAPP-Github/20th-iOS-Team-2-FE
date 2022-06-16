3d3f059dffaec326d6fda361b89183c912e4e60c
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-17 03:52:11 +0900

    [ADD] 이전 button만 있는 Navigation ViewModifier

---
+++ Sofa/Sources/View/Album/AlbumRecordAddView/SubViews/NavigationBar/NavigationBarOnlyCancleButtonStyle.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBarOnlyCancleButtonStyle.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/17.
+//
+
+import SwiftUI
+
+struct NavigationBarOnlyCancleButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationBarOnlyCancleButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarOnlyCancleButtonStyle()
+    }
+}

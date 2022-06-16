8df1c8c2455c7f3bfdc5ead2b216dd54252ef299
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-16 22:56:40 +0900

    [FEAT] Camera picker 보기 전, 권한 확인

--- Sofa/Sources/View/Album/ViewModel/CameraViewModel.swift
+++ Sofa/Sources/View/Album/ViewModel/CameraViewModel.swift
@@ -8,5 +8,18 @@
 import SwiftUI
 
 class CameraViewModel: ObservableObject {
-  
+  @Published var showPicker = false
+  @Published var showErrorAlert = false
+  @Published var cameraError: CameraAuthorization.CameraErrorType?
+    
+  // camera picker 보기 전, 권한 확인
+  func showCameraPicker() {
+    do {
+      try CameraAuthorization.checkPermissions()
+      showPicker = true
+    } catch { // 권한 오류가 발생
+      showErrorAlert = true
+      cameraError = CameraAuthorization.CameraErrorType(error: error as! CameraAuthorization.PickerError)
+    }
+  }
 }

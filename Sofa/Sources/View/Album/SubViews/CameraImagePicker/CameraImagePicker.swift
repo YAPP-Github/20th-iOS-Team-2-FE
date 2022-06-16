2b705cc981f363b0c4ed5407e3fde20ad3f99356
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-16 22:40:29 +0900

    [ADD] ImagePicker 사용을 위한 UIViewControllerRepresentable(UIImagePickerController)

---
+++ Sofa/Sources/View/Album/SubViews/CameraImagePicker/CameraImagePicker.swift
@@ -0,0 +1,25 @@
+//
+//  CameraImagePicker.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/16.
+//
+
+import UIKit
+import SwiftUI
+
+struct CameraImagePicker: UIViewControllerRepresentable {
+
+  
+  func makeUIViewController(context: UIViewControllerRepresentableContext<CameraImagePicker>) -> UIImagePickerController {
+
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CameraImagePicker>) {
+  }
+  
+  func makeCoordinator() -> Coordinator {
+  }
+  
+  
+}

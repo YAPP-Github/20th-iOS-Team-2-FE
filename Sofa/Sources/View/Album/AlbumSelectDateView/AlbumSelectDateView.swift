1112f08e401526182ffc1d417410a0638c0d2c1c
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-16 20:22:52 +0900

    [CHORE] 올리기 버튼을 누르면 화면 dismiss

--- Sofa/Sources/View/Album/AlbumSelectDateView/AlbumSelectDateView.swift
+++ Sofa/Sources/View/Album/AlbumSelectDateView/AlbumSelectDateView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct AlbumSelectDateView: View {
+  @Environment(\.presentationMode) var presentable
   @State var isNext = false
+  @State var isDisalbeNextButton: Bool = false
+  var parent: AlbumPhotoAddView
+  var title: String = "사진 올리기"
+  let buttonColor: Color = Color.init(hex: "#43A047")
   
   var body: some View {
     NavigationView {
       VStack {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
       }
-      .navigationBarWithTextButtonStyle(isNextClick: $isNext, isDisalbeNextButton: .constant(false), "사진 올리기", nextText: "올리기", Color.init(hex: "#43A047"))
+      .navigationBarItems(
+        leading: Button(action: {
+          presentable.wrappedValue.dismiss()
+        }, label: {
+          HStack(spacing: 0) {
+            Image(systemName: "chevron.left")
+            Text("취소")
+          }
+        })
+        .accentColor(buttonColor)
+        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)),
+        trailing: Button(action: {
+          parent.presentable.wrappedValue.dismiss()
+        }, label: {
+          HStack(spacing: 0) {
+            Text("사진 올리기")
+          }
+        })
+        .disabled(isDisalbeNextButton)
+        .accentColor(buttonColor)
+        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+      )
+      .navigationBarTitleDisplayMode(.inline)
+      .navigationTitle(title)
+      .onAppear {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor =
+        UIColor.systemBackground.withAlphaComponent(1)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+      }
     }
     .navigationViewStyle(StackNavigationViewStyle())
     .navigationBarHidden(true)
@@ -24,6 +61,6 @@ struct AlbumSelectDateView: View {
 
 struct AlbumSelectDateView_Previews: PreviewProvider {
   static var previews: some View {
-    AlbumSelectDateView()
+    AlbumSelectDateView(parent: AlbumPhotoAddView())
   }
 }

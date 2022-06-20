d28680111620e615cc6582766bcaa145a4948a07
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-20 16:25:08 +0900

    [ADD] Record view model 추가

---
+++ Sofa/Sources/View/Album/AlbumRecordAddView/SubViews/ViewModel/AudioRecorderViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AudioRecorderViewModel.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/17.
+//
+
+import Foundation
+import AVFoundation
+
+class AudioRecorderViewModel: ObservableObject {
+}

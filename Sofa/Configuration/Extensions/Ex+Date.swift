03f659df9295f6f171ffc074f2de7d7c36dfc80b
임주민 <limj1213@gmail.com>
2022-06-21 21:48:54 +0900

    [ADD] 캘린더 Extension 추가

--- Sofa/Configuration/Extensions/Ex+Date.swift
+++ Sofa/Configuration/Extensions/Ex+Date.swift
@@ -17,3 +17,17 @@ extension Date {
         return dateformat.string(from: self)
     }
 }
+
+extension Date{
+  func getAllDates()->[Date]{
+    let calendar = Calendar.current
+    
+    let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
+    
+    let range = calendar.range(of: .day, in: .month, for: startDate)!
+    
+    return range.compactMap { day -> Date in
+      return calendar.date(byAdding: .day, value: day - 1, to: startDate)!
+    }
+  }
+}

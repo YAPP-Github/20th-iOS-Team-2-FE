7e7cc1b52a23e9b7d6795a5d10badb54db98e963
임주민 <limj1213@gmail.com>
2022-06-21 16:40:31 +0900

    [CHORE] 코드 파일 이동, 수정

--- Sofa/Sources/View/Calendar/CalendarView.swift
+++ Sofa/Sources/View/Calendar/CalendarView.swift
@@ -13,7 +13,31 @@ struct CalendarView: View {
   
   var body: some View {
     ScrollView(.vertical, showsIndicators: false) {
-      VStack(spacing: 20) {
+      VStack(spacing: 0) {
+        HStack(spacing: 0){
+          Text("캘린더")
+            .font(.custom("Pretendard-Bold", size: 24))
+            .foregroundColor(Color(hex: "121619"))
+            .padding(EdgeInsets(top: 0, leading: 24, bottom: 12, trailing: 0))
+          Spacer()
+          Button {
+            
+          } label: {
+            Image(systemName: "plus")
+              .font(.custom("SF-pro", size: 20))
+              .foregroundColor(Color(hex: "43A047"))
+              .padding(EdgeInsets(top: 0, leading: 0, bottom: 12, trailing: 31))
+            
+          }
+        }
+        Rectangle()
+          .frame(height: 1.0, alignment: .bottom)
+          .foregroundColor(Color(hex: "EDEADF"))
+        
+        Color(hex: "FAF8F0")
+          .ignoresSafeArea()
+          .frame(width: .infinity, height: 8, alignment: .center)
+        
         CustomDatePicker(currentDate: $currentDate)
       }
     }
--- Sofa/Sources/View/Calendar/CustomDatePicker.swift
+++ Sofa/Sources/View/Calendar/CustomDatePicker.swift
@@ -10,50 +10,23 @@ import SwiftUI
 struct CustomDatePicker: View {
   
   @Binding var currentDate: Date
+  
   var body: some View {
     VStack(spacing: 0){
-      HStack{
-        Text("캘린더")
-          .font(.custom("Pretendard-Bold", size: 24))
-          .foregroundColor(Color(hex: "121619"))
-          .padding(EdgeInsets(top: 0, leading: 24, bottom: 12, trailing: 0))
-        Spacer()
-        Button {
-          
-        } label: {
-          Image(systemName: "plus")
-            .font(.custom("SF-pro", size: 20))
-            .foregroundColor(Color(hex: "43A047"))
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 12, trailing: 31))
-          
-        }
-      }
-      Rectangle()
-        .frame(height: 1.0, alignment: .bottom)
-        .foregroundColor(Color(hex: "EDEADF"))
-      
-      Color(hex: "FAF8F0")
-        .ignoresSafeArea()
-        .frame(width: .infinity, height: 8, alignment: .center)
-      
       HStack(spacing: 0){
         Text("2022년 6월")
           .font(.custom("Pretendard-Bold", size: 18))
           .foregroundColor(Color(hex: "121619"))
           .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
         Button {
-          
         } label: {
           Image(systemName: "chevron.right")
             .font(.custom("SF-pro", size: 20))
             .foregroundColor(Color(hex: "121619"))
             .padding(EdgeInsets(top: 0, leading: 12.5, bottom: 0, trailing: 0))
         }
-        
         Spacer(minLength: 0)
-        
         Button {
-          
         } label: {
           Image(systemName: "chevron.left")
             .font(.custom("SF-pro", size: 20))
@@ -61,15 +34,13 @@ struct CustomDatePicker: View {
           
         }
         Button {
-          
         } label: {
           Image(systemName: "chevron.right")
             .font(.custom("SF-pro", size: 20))
             .foregroundColor(Color(hex: "121619"))
             .padding(EdgeInsets(top: 0, leading: 13, bottom: 0, trailing: 20.5))
         }
-        
-      }//HStack2
+      }// HStack
       .padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
     }
   }

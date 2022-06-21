c460c04dadc8bf33b601bc1fd2c0b270cac10b76
임주민 <limj1213@gmail.com>
2022-06-21 16:31:37 +0900

    [FEAT] 캘린더 상단바 UI 디테일 구현

--- Sofa/Sources/View/Calendar/CustomDatePicker.swift
+++ Sofa/Sources/View/Calendar/CustomDatePicker.swift
@@ -11,29 +11,66 @@ struct CustomDatePicker: View {
   
   @Binding var currentDate: Date
   var body: some View {
-    VStack(spacing: 35){
-      HStack(spacing: 20){
-        Text("2022년")
-          .font(.caption)
-          .fontWeight(.semibold)
+    VStack(spacing: 0){
+      HStack{
+        Text("캘린더")
+          .font(.custom("Pretendard-Bold", size: 24))
+          .foregroundColor(Color(hex: "121619"))
+          .padding(EdgeInsets(top: 0, leading: 24, bottom: 12, trailing: 0))
+        Spacer()
+        Button {
+          
+        } label: {
+          Image(systemName: "plus")
+            .font(.custom("SF-pro", size: 20))
+            .foregroundColor(Color(hex: "43A047"))
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 12, trailing: 31))
+          
+        }
+      }
+      Rectangle()
+        .frame(height: 1.0, alignment: .bottom)
+        .foregroundColor(Color(hex: "EDEADF"))
+      
+      Color(hex: "FAF8F0")
+        .ignoresSafeArea()
+        .frame(width: .infinity, height: 8, alignment: .center)
+      
+      HStack(spacing: 0){
+        Text("2022년 6월")
+          .font(.custom("Pretendard-Bold", size: 18))
+          .foregroundColor(Color(hex: "121619"))
+          .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
+        Button {
+          
+        } label: {
+          Image(systemName: "chevron.right")
+            .font(.custom("SF-pro", size: 20))
+            .foregroundColor(Color(hex: "121619"))
+            .padding(EdgeInsets(top: 0, leading: 12.5, bottom: 0, trailing: 0))
+        }
         
-        Text("6월")
-          .font(.title.bold())
         Spacer(minLength: 0)
+        
         Button {
           
         } label: {
           Image(systemName: "chevron.left")
-            .font(.title2)
+            .font(.custom("SF-pro", size: 20))
+            .foregroundColor(Color(hex: "121619"))
+          
         }
         Button {
           
         } label: {
           Image(systemName: "chevron.right")
-            .font(.title2)
+            .font(.custom("SF-pro", size: 20))
+            .foregroundColor(Color(hex: "121619"))
+            .padding(EdgeInsets(top: 0, leading: 13, bottom: 0, trailing: 20.5))
         }
-      }//HStack
-      .padding(.horizontal)
+        
+      }//HStack2
+      .padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
     }
   }
 }

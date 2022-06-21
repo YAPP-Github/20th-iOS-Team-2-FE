83e3ca22806301edd6ae1795698a7e7ab9651492
임주민 <limj1213@gmail.com>
2022-06-21 16:54:23 +0900

    [FEAT] 요일 추가

--- Sofa/Sources/View/Calendar/CustomDatePicker.swift
+++ Sofa/Sources/View/Calendar/CustomDatePicker.swift
@@ -13,6 +13,10 @@ struct CustomDatePicker: View {
   
   var body: some View {
     VStack(spacing: 0){
+      
+      // Days
+      let days: [String] = ["일","월","화","수","목","금","토"]
+      
       HStack(spacing: 0){
         Text("2022년 6월")
           .font(.custom("Pretendard-Bold", size: 18))
@@ -40,8 +44,20 @@ struct CustomDatePicker: View {
             .foregroundColor(Color(hex: "121619"))
             .padding(EdgeInsets(top: 0, leading: 13, bottom: 0, trailing: 20.5))
         }
-      }// HStack
+      }
       .padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
+      
+      // Day View
+      HStack(spacing: 0){
+        ForEach(days, id: \.self){day in
+          Text(day)
+            .font(.custom("Pretendard-Medium", size: 13))
+            .frame(maxWidth: .infinity)
+            .padding(EdgeInsets(top: 9, leading: 0, bottom: 0, trailing: 0))
+            .foregroundColor(.black)
+            .opacity(0.4)
+        }
+      }
     }
   }
 }

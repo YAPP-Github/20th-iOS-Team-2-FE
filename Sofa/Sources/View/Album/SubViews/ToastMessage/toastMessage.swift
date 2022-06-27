4b622496cf33d0a7937fc897188c2c68d3fa5ff0
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-27 22:14:56 +0900

    [ADD] Toast message 파일 추가

---
+++ Sofa/Sources/View/Album/SubViews/ToastMessage/toastMessage.swift
@@ -0,0 +1,20 @@
+//
+//  toastMessage.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/27.
+//
+
+import SwiftUI
+
+struct toastMessage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct toastMessage_Previews: PreviewProvider {
+    static var previews: some View {
+        toastMessage()
+    }
+}

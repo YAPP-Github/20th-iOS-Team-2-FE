e61fdea7da929c6ec16cc5bf3e7d4e97cc815d9b
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-06-28 16:44:13 +0900

    [ADD] AlbumImageDetailView 파일 추가

---
+++ Sofa/Sources/View/Album/AlbumImageDetailView/AlbumImageDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumImageDetailView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/06/28.
+//
+
+import SwiftUI
+
+struct AlbumImageDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumImageDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumImageDetailView()
+    }
+}

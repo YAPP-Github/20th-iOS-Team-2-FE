6d452951302e69966b59ff18298a72c62fdbc999
임주민 <limj1213@gmail.com>
2022-06-29 20:30:45 +0900

    [ADD] TextField, placeholder관련 Extension 추가

---
+++ Sofa/Sources/View/Calendar/AppendTaskModalView.swift
@@ -0,0 +1,20 @@
+//
+//  AppendTaskModalView.swift
+//  Sofa
+//
+//  Created by 임주민 on 2022/06/28.
+//
+
+import SwiftUI
+
+struct AppendTaskModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppendTaskModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppendTaskModalView()
+    }
+}

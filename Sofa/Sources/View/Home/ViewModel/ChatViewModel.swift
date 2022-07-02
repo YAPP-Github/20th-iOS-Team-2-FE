f7d8117896183ea18a01695ca19d1e93cc4e64fd
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-07-03 01:17:34 +0900

    [FEAT] ChatInfoMock.json

---
+++ Sofa/Sources/View/Home/ViewModel/ChatViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewModel.swift
+//  Sofa
+//
+//  Created by 양유진 on 2022/07/03.
+//
+
+import SwiftUI
+
+struct ChatViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatViewModel()
+    }
+}

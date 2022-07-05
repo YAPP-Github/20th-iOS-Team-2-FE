2d9f64f13d6b03bef65688045a1bfaadf0cd5790
임주민 <limj1213@gmail.com>
2022-07-05 21:39:51 +0900

    [CHORE] 애니메이션 추가

---
+++ Sofa/Sources/View/Calendar/SubViews/TaskColorPicker.swift
@@ -0,0 +1,20 @@
+//
+//  TaskColorPicker.swift
+//  Sofa
+//
+//  Created by 임주민 on 2022/07/05.
+//
+
+import SwiftUI
+
+struct TaskColorPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskColorPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskColorPicker()
+    }
+}

d74b31f9b9819d26df4c730ca105c16ecbcdc202
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-10 19:01:21 +0900

    [ADD] Modal의 배경(out of Focus Area) 추가

---
+++ Sofa/Sources/View/Album/AlbumCommentView/SubViews/CommentModal/ModalBackGround.swift
@@ -0,0 +1,20 @@
+//
+//  ModalBackGround.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/07/08.
+//
+
+import SwiftUI
+
+struct ModalBackGround: View {  
+  var body: some View {
+
+  }
+}
+
+struct ModalBackGround_Previews: PreviewProvider {
+  static var previews: some View {
+    ModalBackGround()
+  }
+}

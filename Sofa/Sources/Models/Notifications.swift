2465f46c137e2a60802f6ef08bb444c633675af8
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-07-10 14:41:37 +0900

    [FEAT] Notification Model

---
+++ Sofa/Sources/Models/Notifications.swift
@@ -0,0 +1,31 @@
+//
+//  Notifications.swift
+//  Sofa
+//
+//  Created by 양유진 on 2022/07/10.
+//
+
+import Foundation
+
+struct Notifications: Decodable {
+  var notifications: [Notification]
+}
+
+struct Notification: Decodable {
+  var type: String
+  var user: String
+  var targetId: Int
+  var createdDate: String
+  
+  static func getDummy() -> Self{
+    return Notification(type: "CALENDAR", user: "엄마 아빠 딸", targetId: 3, createdDate: "2022-06-05 22:30")
+  }
+  
+  var descriptionUser: String{
+    return "\(user)"
+  }
+  
+  var descriptionDate: String{
+    return "\(createdDate)"
+  }
+}
---
+++ Sofa/Sources/View/Home/ViewModel/NotificationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationViewModel.swift
+//  Sofa
+//
+//  Created by 양유진 on 2022/07/10.
+//
+
+import Foundation

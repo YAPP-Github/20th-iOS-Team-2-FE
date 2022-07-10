d59703bb10d03600654ac3b4337878ffc58738c9
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-10 22:04:22 +0900

    [FEAT] ScrollView + LazyVStack으로 댓글 List UI 구현

--- Sofa/Sources/View/Album/AlbumCommentView/SubViews/Row/AlbumCommentList.swift
+++ Sofa/Sources/View/Album/AlbumCommentView/SubViews/Row/AlbumCommentList.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct AlbumCommentList: View {
+  @ObservedObject var viewModel = CommentViewModel()
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ScrollView {
+      // 필요할때 rendering 함, network에 적합
+      LazyVStack(spacing: 0) {
+        ForEach(Array(zip(viewModel.comments.indices, viewModel.comments)), id: \.0) { index, element in
+          AlbumCommentRow(comment: element)
+            .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
+        }
+      }
+    }
+    .onAppear {
+      self.viewModel.fetchComment()
+    }
   }
 }
 

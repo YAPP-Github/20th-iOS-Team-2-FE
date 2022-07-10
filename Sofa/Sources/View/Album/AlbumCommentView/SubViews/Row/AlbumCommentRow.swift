e217f1c17a85e0fdbc2cc8f04d7cf205aaba2f30
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-10 19:09:11 +0900

    [ADD] 댓글 Row 파일 추가

---
+++ Sofa/Sources/View/Album/AlbumCommentView/SubViews/Row/AlbumCommentRow.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumCommentRow.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/07/10.
+//
+
+import SwiftUI
+
+struct AlbumCommentRow: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct AlbumCommentRow_Previews: PreviewProvider {
+  static var previews: some View {
+    AlbumCommentRow()
+  }
+}

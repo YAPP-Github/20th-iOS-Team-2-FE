5f33a80c4e0dd54ac6cbf3438d26f6c8fe8683d7
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-10 20:20:27 +0900

    [FEAT] Comment row UI 구현

--- Sofa/Sources/View/Album/AlbumCommentView/SubViews/Row/AlbumCommentRow.swift
+++ Sofa/Sources/View/Album/AlbumCommentView/SubViews/Row/AlbumCommentRow.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct AlbumCommentRow: View {
+  var comment: Comment
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    HStack(alignment: .top, spacing: 16) {
+      Image("lionprofile") // 이미지
+        .resizable()
+        .frame(width: 48, height: 48)
+        .padding(EdgeInsets(top: 12, leading: 12, bottom: 0, trailing: 0))
+      
+      VStack(alignment: .leading) { // 댓글 정보
+        HStack(alignment: .center, spacing: 8) { // 댓글 작성자 정보
+          Text("\(comment.nickname)") // 별명
+            .font(.custom("Pretendard-Bold", size: 13))
+          
+          Text("\(comment.roleInFamily)") // 역할
+            .font(.custom("Pretendard-Medium", size: 12))
+            .padding(EdgeInsets(top: 1, leading: 8, bottom: 1, trailing: 8))
+            .background(Color(hex: "E8F5E9"))
+            .foregroundColor(Color(hex: "43A047"))
+            .cornerRadius(4)
+          
+          Spacer()
+          
+          Text("\(comment.descriptionDate)") // 날짜
+            .font(.custom("Pretendard-Medium", size: 13))
+            .foregroundColor(Color.secondary)
+            .padding(.trailing, 16)
+        }
+        .padding(.top, 12)
+        
+        Text("\(comment.content)") // 댓글
+          .font(.custom("Pretendard-Medium", size: 14))
+          .lineSpacing(5)
+      }
+    }
   }
 }
 
 struct AlbumCommentRow_Previews: PreviewProvider {
   static var previews: some View {
-    AlbumCommentRow()
+    AlbumCommentRow(comment: Comment.getDummy())
   }
 }

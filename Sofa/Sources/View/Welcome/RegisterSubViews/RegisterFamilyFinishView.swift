67589020de3a395fabc7563472694557725c704f
임주민 <limj1213@gmail.com>
2022-07-14 14:29:27 +0900

    [FEAT] 가족 공간 만들기 UI 구현

--- Sofa/Sources/View/Welcome/RegisterSubViews/RegisterFamilyFinishView.swift
+++ Sofa/Sources/View/Welcome/RegisterSubViews/RegisterFamilyFinishView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct RegisterFamilyFinishView: View {
     var body: some View {
-        Text("가족 공간 생성 완료")
+      
+      VStack(spacing: 0){
+        ZStack{
+          Rectangle()
+            .ignoresSafeArea()
+            .frame(height: 191-34)
+            .foregroundColor(Color(hex: "#388E3C"))
+          
+          VStack(spacing: 0){
+            Text("가족 공간 생성 완료!")
+              .font(.custom("Pretendard-Bold", size: 28))
+              .foregroundColor(Color.white)
+              .frame(height: 42)
+
+            Text("우리 가족만의 공간이 생성되었어요.")
+              .font(.custom("Pretendard-Medium", size: 20))
+              .foregroundColor(Color.white.opacity(0.5))
+              .padding(.top, 8)
+              .frame(height: 30)
+          }
+        }
+        
+        Button(action: {
+
+        }){
+          Image("linkBtn")
+        }
+        .padding(.top, 70)
+        
+        Spacer()
+        Button(action: {
+//          self.showFamilyRegister = true
+        }){
+          Image("goRoomBtn")
+        }
+//        .fullScreenCover(isPresented: $showFamilyRegister, content: RegisterFamilyView.init)
+        .padding(.bottom, 88-34)
+        
+        
+        
+      }
     }
 }
 

6abf1e2bdaf480329fb611c5acf8d4db5d89e68b
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-15 21:42:51 +0900

    [ADD] AlbumTitleEditView 파일 추가

---
+++ Sofa/Sources/View/Album/AlbumTitleEditView/AlbumTitleEditView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumTitleEditView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/07/15.
+//
+
+import SwiftUI
+
+struct AlbumTitleEditView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct AlbumTitleEditView_Previews: PreviewProvider {
+  static var previews: some View {
+    AlbumTitleEditView()
+  }
+}

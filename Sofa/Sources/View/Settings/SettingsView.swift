32df049dee0d5215cde2b4c85ee1347994eb3a29
임주민 <limj1213@gmail.com>
2022-07-16 01:06:47 +0900

    [CHORE] 설정뷰 기본 세팅

--- Sofa/Sources/View/Settings/SettingsView.swift
+++ Sofa/Sources/View/Settings/SettingsView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct SettingsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var body: some View {
+    NavigationView {
+      ZStack{
+        Color(hex: "FAF8F0").ignoresSafeArea()
+        VStack(spacing: 0){
+          Rectangle()
+            .frame(height: 1.0, alignment: .bottom)
+            .foregroundColor(Color(hex: "EDEADF"))
+          
+          //첫번재 단락
+          HStack{
+            VStack{
+              //글씨1
+              //글씨2
+            }
+            //버튼
+          }
+          HStack{
+            //사진
+            VStack{
+              //글씨1
+              //글씨2
+            }
+            //버튼
+          }
+          
+          //두번째 단락
+    
+          
+          Spacer()
+          
+        }///VStack
+        .navigationBarWithIconButtonStyle(isButtonClick: .constant(false), buttonColor: Color.clear, "설정", "")
+      }///ZStack
+    }///NavigationView
+  }
 }
 
 struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-    }
+  static var previews: some View {
+    SettingsView()
+  }
 }

0db06cef48012a5b051604d1b1fba69e68e1eab5
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-16 21:47:28 +0900

    [ADD] 날짜 수정 UI 파일 추가

---
+++ Sofa/Sources/View/Album/AlbumEditDateView/AlbumEditDateView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumEditDateView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/07/16.
+//
+
+import SwiftUI
+
+struct AlbumEditDateView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct AlbumEditDateView_Previews: PreviewProvider {
+  static var previews: some View {
+    AlbumEditDateView()
+  }
+}

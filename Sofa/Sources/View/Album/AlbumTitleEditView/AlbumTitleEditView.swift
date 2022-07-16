37999196b0b26bfc70c4c720d5fe80454fb73cfd
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-16 14:58:53 +0900

    [FEA] Album title 수정 UI

--- Sofa/Sources/View/Album/AlbumTitleEditView/AlbumTitleEditView.swift
+++ Sofa/Sources/View/Album/AlbumTitleEditView/AlbumTitleEditView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct AlbumTitleEditView: View {
+  @StateObject var keyboardHeightHelper = KeyboardHeightHelper()
+  let duration = 0.5
+    .offset(y: -keyboardHeightHelper.keyboardWillShowHeight)
   var body: some View {
     Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+          DispatchQueue.main.asyncAfter(deadline: .now() + duration/2) {
+            // isTextView가 닫히고
+            self.isShowing = false
+          }
+            .transition(.move(edge: .top))
+            .animation(.easeInOut(duration: duration/2))
+            .transition(.move(edge: .bottom))
+            .animation(.easeInOut(duration: duration/2))
+      .onAppear {
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+          // 0.5초 후 isTextView Show
+          self.isTextView = true
+        }
+      }
   }
 }
 

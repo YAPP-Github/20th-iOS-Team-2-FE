757e7b39c77ef4ca91067f52c5e819ec324fa0cc
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-17 00:45:21 +0900

    [ADD] Album Record Detail UI 파일 추가

---
+++ Sofa/Sources/View/Album/AlbumRecordDetailView/AlbumRecordDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumRecordDetailView.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/07/17.
+//
+
+import SwiftUI
+
+struct AlbumRecordDetailView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct AlbumRecordDetailView_Previews: PreviewProvider {
+  static var previews: some View {
+    AlbumRecordDetailView()
+  }
+}

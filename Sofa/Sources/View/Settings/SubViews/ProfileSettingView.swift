f556efea389d39ed9435f6486a7876d8ebfa8d84
임주민 <limj1213@gmail.com>
2022-07-16 21:40:12 +0900

    [FEAT] 상단 네비게이션 바 추가

--- Sofa/Sources/View/Settings/SubViews/ProfileSettingView.swift
+++ Sofa/Sources/View/Settings/SubViews/ProfileSettingView.swift
@@ -8,37 +8,43 @@
 import SwiftUI
 
 struct ProfileSettingView: View {
-    var body: some View {
-      NavigationView {
-        ZStack{
-          //기본 배경
+  
+  @State var isChangeProfile: Bool = false
+  @State var isDisableNextButton: Bool = false
+  
+  var body: some View {
+    NavigationView {
+      ZStack{
+        //기본 배경
+        
+        VStack{
+          //사진
           
-          VStack{
-            //사진
-            
-            Spacer()
-          }
-          VStack{
-            //초록 배경
-            
-            //흰 배경
-            
-            //입력창 - 별명
-            
-            //입력창 - 성함
-            
-            //입력창 - 역할
-            
-            //입력창 - 생년월일
-            
-          }
+          Spacer()
         }
-      }
+        VStack{
+          //초록 배경
+          
+          //흰 배경
+          
+          //입력창 - 별명
+          
+          //입력창 - 성함
+          
+          //입력창 - 역할
+          
+          //입력창 - 생년월일
+          
+        }
+      }.navigationBarWithTextButtonStyle(isNextClick: $isChangeProfile, isDisalbeNextButton: $isDisableNextButton, "프로필", nextText: "수정", Color.init(hex: "#43A047"))
     }
+    
+  }
 }
 
+
 struct ProfileSettingView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileSettingView()
-    }
+  static var previews: some View {
+    ProfileSettingView()
+  }
 }

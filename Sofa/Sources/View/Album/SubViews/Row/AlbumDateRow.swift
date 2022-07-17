32a2b9a747a882e3cbe1ba68f29a5e90de373acf
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-17 16:45:50 +0900

    [CHORE] 날짜별 Row 글꼴 및 색상 수정

--- Sofa/Sources/View/Album/SubViews/Row/AlbumDateRow.swift
+++ Sofa/Sources/View/Album/SubViews/Row/AlbumDateRow.swift
@@ -11,7 +11,7 @@ struct AlbumDateRow: View {
   var album: AlbumDate
   
   var body: some View {
-    HStack(alignment:.top, spacing: 8) { // 위로 붙임
+    HStack(alignment:.top, spacing: 16) { // 위로 붙임
       // 썸네일
       Image(album.thumbnail)
         .resizable()
@@ -21,13 +21,13 @@ struct AlbumDateRow: View {
       // 제목
       VStack(alignment: .leading, spacing: 3) {
         Text(album.title == "" ? "\(album.date) 앨범" : album.title)
-          .font(.system(size: 16, weight: .semibold))
-          .foregroundColor(Color.black)
+          .font(.custom("Pretendard-Bold", size: 16))
+          .foregroundColor(Color(hex: "#121619"))
           .lineLimit(2)
         
         Text(album.date)
-          .font(.subheadline)
-          .foregroundColor(.secondary)
+          .font(.custom("Pretendard-Medium", size: 13))
+          .foregroundColor(Color(hex: "999999"))
       }
       
       Spacer()
@@ -41,7 +41,7 @@ struct AlbumDateRow: View {
     }
     .padding(8)
     .background(Color.white)
-    .cornerRadius(12)
+    .cornerRadius(8)
     .fixedSize(horizontal: false, vertical: true)
   }
 }

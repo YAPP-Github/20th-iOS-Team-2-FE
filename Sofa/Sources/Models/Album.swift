be2ec6fcb6c0e070645e4fbe6727d04361724b30
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-17 16:17:58 +0900

    [FEAT] pagenation을 위한 model 추가

--- Sofa/Sources/Models/Album.swift
+++ Sofa/Sources/Models/Album.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct AlbumDateAPIResponse: Hashable, Decodable {
+  let info: Info
   let albums: [AlbumDate]
 }
 
@@ -28,4 +29,9 @@ struct AlbumType: Hashable, Decodable {
   let kind: String
   let count: Int
   let link: String
+
+struct Info: Hashable, Decodable {
+  let totalCount: Int // pagination이 적용안된 전체 element 개수
+  let pageCount: Int  // pagination이 적용된 전체 element의 페이지 개수
+  let pageSize: Int   // 한 page에 들어간 element 개수
 }

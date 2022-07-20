fa143115fa797841b1a5cbfc828b1e8eb17145fc
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-20 19:44:45 +0900

    [FEAT] Upload Manager (파일 업로드 후, http link 받아오기)

---
+++ Sofa/Sources/Network/UploadManager.swift
@@ -0,0 +1,53 @@
+//
+//  UploadManager.swift
+//  Sofa
+//
+//  Created by geonhyeong on 2022/07/19.
+//
+
+import Alamofire
+import Combine
+
+enum UploadManager: URLRequestConvertible {
+  
+  case postImages
+  
+  var baseURL: URL {
+    switch self {
+    case .postImages:
+      return URL(string: "\(APIConstants.url)/files")!
+    }
+  }
+  
+  var method: HTTPMethod {
+    switch self {
+    case .postImages:
+      return .post
+    }
+  }
+  
+  var headers: HTTPHeaders {
+    var headers = HTTPHeaders()
+    let accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJLQUtBTzoyMTczNzMzODA0IiwiaWF0IjoxNjU4MDM4NzA0LCJleHAiOjE2NjU4MTQ3MDR9.Cm1pEFN83ribamFh36WdnSTJI74Crmy2T9XmxElwr1Q"
+    headers["Authorization"] = accessToken
+    
+    switch self {
+    case .postImages:
+      headers["Content-Type"] = "multipart/form-data"
+      return headers
+    }
+  }
+  
+  func asURLRequest() throws -> URLRequest {
+    let url = baseURL
+    
+    var request = URLRequest(url: url)
+    request.method = method
+    request.headers = headers
+    
+    request = try URLEncoding.default.encode(request, with: nil)
+    
+    return request
+  }
+}
+

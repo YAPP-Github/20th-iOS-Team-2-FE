ec8d3b574d756af3c558532c40a0f7c1c6334fe5
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-20 19:43:29 +0900

    [FEAT] UploadFiles API Response model + Photo, SelectedImages 파일 합병

--- Sofa/Sources/Models/UploadFile.swift
+++ Sofa/Sources/Models/UploadFile.swift
@@ -1,13 +1,17 @@
 //
-//  Photo.swift
+//  UploadFile.swift
 //  Sofa
 //
-//  Created by geonhyeong on 2022/06/12.
+//  Created by geonhyeong on 2022/07/19.
 //
 
 import Foundation
 import SwiftUI
 import Photos
+
+struct UploadFilesAPIResponse: Hashable, Decodable {
+  let links: [String]
+}
 
 struct Photo: Hashable {
   var isSelect: Bool

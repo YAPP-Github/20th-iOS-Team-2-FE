4f50e58f2f050546045a22b2a17b780c973f656b
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-21 23:11:09 +0900

    [FEAT] PullToRefresh

--- Sofa/Sources/View/Album/SubViews/ScrollViewHelper/PullToRefresh.swift
+++ Sofa/Sources/View/Album/SubViews/ScrollViewHelper/PullToRefresh.swift
@@ -5,4 +5,39 @@
 //  Created by geonhyeong on 2022/07/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct PullToRefresh: View {
+  var coordinateSpaceName: String
+  var onRefresh: () -> Void
+  
+  @State var needRefresh: Bool = false
+  
+  var body: some View {
+    GeometryReader { geo in
+      if (geo.frame(in: .named(coordinateSpaceName)).midY > 50) {
+        Spacer()
+          .onAppear {
+            needRefresh = true
+          }
+      } else if (geo.frame(in: .named(coordinateSpaceName)).maxY < 10) {
+        Spacer()
+          .onAppear {
+            if needRefresh {
+              needRefresh = false
+              onRefresh()
+            }
+          }
+      }
+      HStack {
+        Spacer()
+        if needRefresh {
+          ProgressView()
+        } else {
+//          Text("⬇️")
+        }
+        Spacer()
+      }
+    }.padding(.top, -50)
+  }
+}

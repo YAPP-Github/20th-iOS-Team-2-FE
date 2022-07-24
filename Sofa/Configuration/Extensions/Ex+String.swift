2d1b137772489250c1d95f3bf7b0f5ae6061344f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-25 01:07:37 +0900

    [FIX] 날짜 Date 형식이 24시간으로 표기되어 변환시 nil을 반환하는 문제 해결

--- Sofa/Configuration/Extensions/Ex+String.swift
+++ Sofa/Configuration/Extensions/Ex+String.swift
@@ -22,7 +22,7 @@ extension String {
   
   func toDateIncludeT() -> Date? { // "yyyy-MM-ddTHH:mm:ss"
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss"
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
     dateFormatter.timeZone = TimeZone(identifier: "ko_KR")
     
     if let date = dateFormatter.date(from: self) {

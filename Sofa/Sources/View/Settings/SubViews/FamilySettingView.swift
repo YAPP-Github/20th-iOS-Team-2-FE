a7c8e7770bfef78ca9d1b8becd8b981ddc3ecf81
임주민 <limj1213@gmail.com>
2022-07-27 20:27:39 +0900

    [FEAT] 가족 설정 뷰 구현

--- Sofa/Sources/View/Settings/SubViews/FamilySettingView.swift
+++ Sofa/Sources/View/Settings/SubViews/FamilySettingView.swift
@@ -12,10 +12,52 @@ struct FamilySettingView: View {
   @Environment(\.presentationMode) var presentable
   @ObservedObject var tabbarManager = TabBarManager.shared
   
+  @State var familyName: String
+  @State var familyMotto: String
+  @State var familyInfoFocused = [false, false]
+  var familyMemberName = ["엄마", "아빠", "오빠", "언니"]
+  let familyCount = 4
+  
     var body: some View {
       NavigationView {
         ZStack{
-          
+          //기본 배경
+          Color(hex: "FAF8F0").ignoresSafeArea()
+          ScrollView{
+            VStack(spacing: 0) {
+              Border()
+              
+              ZStack{
+                Color.white
+                VStack(spacing: 0){
+                  profileText(text: "가족공간명")
+                    .padding(.top, 16)
+                  profileTextField(text: $familyName, isFocused: $familyInfoFocused[0], showingSheet: .constant(false))
+                  profileText(text: "가훈")
+                  profileTextField(text: $familyMotto, isFocused: $familyInfoFocused[1], showingSheet: .constant(false))
+                }
+              }.padding(.top, 8)
+              Border()
+              
+              ZStack{
+                Color.white
+                VStack(spacing: 0) {
+                  HStack{
+                    Text("가족 구성원")
+                      .padding(.leading, 16)
+                      .padding(.top, 16)
+                      .font(.custom("Pretendard-Bold", size: 14))
+                    Spacer()
+                  }
+                  ForEach(0..<familyCount) {idx in
+                    FamilyMemberRow(nickName: familyMemberName[idx])
+                  }
+                }
+              }.padding(.top, 8)
+              Border()
+              
+            }
+          }
         }
         .navigationBarItems(
           leading: Button(action: {
@@ -63,8 +105,56 @@ struct FamilySettingView: View {
     }
 }
 
+struct Border: View {
+  var body: some View {
+    Rectangle()
+      .frame(height: 1)
+      .foregroundColor(Color(hex: "#EDEADF"))
+  }
+}
+
+struct FamilyMemberRow: View {
+  @State var nickName: String
+  @State var isFocused = false
+  
+  var body: some View {
+    HStack(spacing: 0){
+      ZStack{
+        //이미지
+        RoundedRectangle(cornerRadius: 9)
+          .frame(width: 56, height: 56)
+          .foregroundColor(Color(hex: "#F5F2E9"))
+      }
+      VStack(spacing: 0){
+        HStack(spacing: 0){
+          //성함
+          Text("성함")
+            .font(.custom("Pretendard-Bold", size: 13))
+            .padding(.leading, 24)
+          Spacer()
+          //관계
+          Rectangle()
+            .frame(width: 37, height: 20)
+            .cornerRadius(4)
+            .foregroundColor(Color(hex: "#F1F8E9"))
+            .overlay(
+              Text("관계")
+                .font(.custom("Pretendard-Bold", size: 12))
+                .foregroundColor(Color(hex:"#558B2F"))
+                .frame(width: 21, height: 18)
+            )
+        }
+        //별명
+        profileTextField(text: $nickName, isFocused: $isFocused, showingSheet: .constant(false))
+      }
+    }
+    .padding(.horizontal, 16)
+    .padding(.vertical, 8)
+  }
+}
+
 struct FamilySettingView_Previews: PreviewProvider {
     static var previews: some View {
-        FamilySettingView()
+        FamilySettingView(familyName: "우리가족 공간", familyMotto: "밥은 잘 챙겨먹자!")
     }
 }

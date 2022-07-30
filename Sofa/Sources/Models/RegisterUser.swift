2a5be624dc13f9d7f8a25f9d2e8178f3cb57ad01
임주민 <limj1213@gmail.com>
2022-07-30 20:31:52 +0900

    [CHORE] 필요없는 더미 생성 코드 삭제

--- Sofa/Sources/Models/RegisterUser.swift
+++ Sofa/Sources/Models/RegisterUser.swift
@@ -12,19 +12,11 @@ struct RegisterUser: Decodable {
   let roleInFamily: String
   let birthDay: String
   let nickname: String
-  
-  static func getDummy() -> Self{
-    return RegisterUser(name: "홍길동", roleInFamily: "아들", birthDay: "1960-02-02", nickname: "홍길동")
-  }
 }
 
 struct RegisterFamily: Decodable {
   let familyName: String
   let familyMotto: String
-  
-  static func getDummy() -> Self{
-    return RegisterFamily(familyName: "우리가족 짱짱", familyMotto: "착하게 살자")
-  }
 }
 
 struct SimpleUser: Decodable {

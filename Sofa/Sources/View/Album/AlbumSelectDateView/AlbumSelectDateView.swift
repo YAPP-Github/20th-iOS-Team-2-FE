d67544aaf77bef8dc263cd15a8e359a091ed4437
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-07-31 00:44:06 +0900

    [CHORE] 디자인 수정 - 가장 위 Divider 추가

--- Sofa/Sources/View/Album/AlbumSelectDateView/AlbumSelectDateView.swift
+++ Sofa/Sources/View/Album/AlbumSelectDateView/AlbumSelectDateView.swift
@@ -28,94 +28,97 @@ struct AlbumSelectDateView: View {
   
   var body: some View {
     NavigationView {
-      ScrollView {
-        VStack(spacing: 0) {
-          if recordParent != nil { // 녹음일 경우
-            VStack(spacing: 0) {
-              Divider()
-              Spacer() // 임시 - 여백용
-                .frame(height: 8)
-              HStack {
-                Spacer()
-                TextField(recordUrl!.lastPathComponent.split(separator: ".").first!, text: $recordTitle)
-                  .padding(16)
-                  .background(Color.init(hex: "#FAF8F0")) // 임시
-                  .font(.custom("Pretendard-Medium", size: 18))
-                  .foregroundColor(Color.black)
-                  .cornerRadius(6)
-                  .modifier(DismissingKeyboard())
-                Spacer()
+      VStack(spacing: 0) {
+        Divider()
+        ScrollView {
+          VStack(spacing: 0) {
+            if recordParent != nil { // 녹음일 경우
+              VStack(spacing: 0) {
+                Spacer() // 임시 - 여백용
+                  .frame(height: 8)
+                HStack {
+                  Spacer()
+                  TextField(recordUrl!.lastPathComponent.split(separator: ".").first!, text: $recordTitle)
+                    .padding(16)
+                    .background(Color.init(hex: "#FAF8F0")) // 임시
+                    .font(.custom("Pretendard-Medium", size: 18))
+                    .foregroundColor(Color.black)
+                    .cornerRadius(6)
+                    .modifier(DismissingKeyboard())
+                  Spacer()
+                }
+                .frame(width: Screen.maxWidth, height: 80) // 임시 - 높이
+                .background(Color.white)
+                Divider()
               }
-              .frame(width: Screen.maxWidth, height: 80) // 임시 - 높이
-              .background(Color.white)
             }
+            Spacer() // 임시 - 여백용
+              .frame(height: 8)
+            Group {
+              GeneralDatePickerView(showDatePicker: .constant(true), enableToggle: .constant(false), currentDate: $currentDate)
+            }
+            .padding(EdgeInsets(top: 16, leading: 16, bottom: 12, trailing: 16))
+            .background(Color.white)
+            Divider()
+            Spacer()
           }
-          Divider()
-          Spacer() // 임시 - 여백용
-            .frame(height: 8)
-          Group {
-            GeneralDatePickerView(showDatePicker: .constant(true), enableToggle: .constant(false), currentDate: $currentDate)
-          }
-          .padding(EdgeInsets(top: 16, leading: 16, bottom: 12, trailing: 16))
-          .background(Color.white)
-          Divider()
-          Spacer()
         }
-      }
-      .modifier(DismissingKeyboard())
-      .background(Color.init(hex: "#FAF8F0")) // 임시
-      .navigationBarItems(
-        leading: Button(action: {
-          if photoParent == nil && recordParent == nil { // 카메라로 들어왔을 경우,
-            isCameraCancle = true // 카메라 imagePicker로 이동
-          }
-          presentable.wrappedValue.dismiss()
-        }, label: {
-          HStack(spacing: 0) {
-            Image(systemName: "chevron.left")
-            Text("이전")
-              .font(.custom("Pretendard-Medium", size: 16))
-              .fontWeight(.semibold)
-          }
-        })
-        .accentColor(buttonColor)
-        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)),
-        trailing: Button(action: {
-          var recordTitle = ""
-          if photoParent != nil { // 갤러리 사진들
-            photoParent?.presentable.wrappedValue.dismiss()
-          } else if recordParent != nil { // 녹음
-            recordParent?.presentable.wrappedValue.dismiss()
-            recordTitle = String(recordUrl!.lastPathComponent.split(separator: ".").first!)
-          } else { // 카메라
+        .modifier(DismissingKeyboard())
+        .background(Color.init(hex: "#FAF8F0")) // 임시
+        .navigationBarItems(
+          leading: Button(action: {
+            if photoParent == nil && recordParent == nil { // 카메라로 들어왔을 경우,
+              isCameraCancle = true // 카메라 imagePicker로 이동
+            }
             presentable.wrappedValue.dismiss()
-          }
-          tabbarManager.showTabBar = true
-          let dateStr = currentDate.getFormattedDate(format: "yyyy-MM-dd") + "T" + Date().getFormattedDate(format: "hh:mm:ss")
-          viewModel.fetchUploadFiles(date: dateStr, images: images, title: self.recordTitle == "" ? recordTitle : self.recordTitle, audio: recordUrl) // 업로드
-          UITabBar.showTabBar()
-        }, label: {
-          HStack(spacing: 0) {
-            Text("올리기")
-              .font(.custom("Pretendard-Medium", size: 16))
-              .fontWeight(.semibold)
-          }
-        })
-        .accentColor(buttonColor)
-        .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
-      )
-      .navigationBarTitleDisplayMode(.inline)
-      .navigationTitle(title)
-      .onAppear {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithTransparentBackground()
-        appearance.backgroundColor =
-        UIColor.systemBackground.withAlphaComponent(1)
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-      }
-      .edgesIgnoringSafeArea([.bottom]) // Bottom만 safeArea 무시
+          }, label: {
+            HStack(spacing: 0) {
+              Image(systemName: "chevron.left")
+              Text("이전")
+                .font(.custom("Pretendard-Medium", size: 16))
+                .fontWeight(.semibold)
+            }
+          })
+          .accentColor(buttonColor)
+          .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)),
+          trailing: Button(action: {
+            var recordTitle = ""
+            if photoParent != nil { // 갤러리 사진들
+              photoParent?.presentable.wrappedValue.dismiss()
+            } else if recordParent != nil { // 녹음
+              recordParent?.presentable.wrappedValue.dismiss()
+              recordTitle = String(recordUrl!.lastPathComponent.split(separator: ".").first!)
+            } else { // 카메라
+              presentable.wrappedValue.dismiss()
+            }
+            tabbarManager.showTabBar = true
+            let dateStr = currentDate.getFormattedDate(format: "yyyy-MM-dd") + "T" + Date().getFormattedDate(format: "hh:mm:ss")
+            viewModel.fetchUploadFiles(date: dateStr, images: images, title: self.recordTitle == "" ? recordTitle : self.recordTitle, audio: recordUrl) // 업로드
+            UITabBar.showTabBar()
+          }, label: {
+            HStack(spacing: 0) {
+              Text("올리기")
+                .font(.custom("Pretendard-Medium", size: 16))
+                .fontWeight(.semibold)
+            }
+          })
+          .accentColor(buttonColor)
+          .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+        )
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle(title)
+        .onAppear {
+          let appearance = UINavigationBarAppearance()
+          appearance.configureWithTransparentBackground()
+          appearance.backgroundColor =
+          UIColor.systemBackground.withAlphaComponent(1)
+          UINavigationBar.appearance().standardAppearance = appearance
+          UINavigationBar.appearance().compactAppearance = appearance
+          UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
+        .edgesIgnoringSafeArea([.bottom]) // Bottom만 safeArea 무시
+
+      } // NvigationView
     }
     .navigationBarHidden(true)
   }

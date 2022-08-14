45dae2a2ba15ab9c69332d348e669c94929479f0
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-08-15 02:39:22 +0900

    [TEST] navigationBar background Image

--- Sofa/Configuration/Extensions/Ex+UINavigationController.swift
+++ Sofa/Configuration/Extensions/Ex+UINavigationController.swift
@@ -11,7 +11,13 @@ import UIKit
 
 extension UINavigationController { // Backbutton Custom
   open override func viewWillLayoutSubviews() {
-    navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "이전", style: .plain, target: nil, action: nil)
+    let yourBackImage = UIImage(systemName: "arrow.left")
+//    self.navigationController?.navigationBar.backIndicatorImage = yourBackImage
+//    self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = yourBackImage
+//    self.navigationController?.navigationBar.backItem?.title = "Custom"
+    navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    navigationBar.backIndicatorImage = yourBackImage
+    navigationBar.backIndicatorTransitionMaskImage = yourBackImage
     navigationBar.barTintColor = .white
     // navigationBar 아래 line 투명하게
     navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)

76985be1dafb65b7efb14c90ef5f4fc498b00414
GeonhyeongKim <geonhyeong.dev@gmail.com>
2022-08-28 17:56:21 +0900

    [CHORE] 서버의 create date 형식 변경으로 인한 수정

--- Sofa/Sources/Models/Comment.swift
+++ Sofa/Sources/Models/Comment.swift
@@ -22,11 +22,11 @@ struct Comment: Decodable {
   var profileLink: String
   var nickname: String
   var roleInFamily: String
-  var createdDate: String
+  var created: String
   var content: String
   
   static func getDummy() -> Self{
-    return Comment(commentId: 0, writerId: 0, profileLink: "", nickname: "별명", roleInFamily: "엄마", createdDate: "2022-07-04 20:20:00", content: "또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다 \n댓글에 \n계속 \n줄바꿈을 \n누를 \n때 \n별다른 \n제한은 \n없다.\n\n\n\n")
+    return Comment(commentId: 0, writerId: 0, profileLink: "", nickname: "별명", roleInFamily: "엄마", created: "2022-07-04 20:20:00", content: "또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다또 놀러가고싶다 \n댓글에 \n계속 \n줄바꿈을 \n누를 \n때 \n별다른 \n제한은 \n없다.\n\n\n\n")
   }
   
   // 마지막 공백 제거하여 반환
@@ -42,7 +42,8 @@ struct Comment: Decodable {
   
   // 시간 차이로 나타내기
   var descriptionDate: String {
-    let diff = Date().timeIntervalSince(createdDate.toDate() ?? Date())
+    let split = created.split(separator: ".").map{String($0)}.first!
+    let diff = Date().timeIntervalSince(split.toDateIncludeT() ?? Date())
     
     switch diff {
     case 0..<60:
@@ -54,7 +55,7 @@ struct Comment: Decodable {
     case 86400..<604800: // 이번주 내
       return "\(Int(diff/86400))일 전"
     default:
-      return createdDate.split(separator: " ").map{String($0)}.first!
+      return split.split(separator: "T").map{String($0)}.first!
     }
   }
 }
